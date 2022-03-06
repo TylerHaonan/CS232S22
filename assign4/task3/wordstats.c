3d9b43c2cee79d401f6fc5dcd7f0ff8db621245f
tylerHaoW <100734889+TylerHaonan@users.noreply.github.com>
2022-03-06 02:13:57 +0000

    fighting

--- assign4/task3/wordstats.c
+++ assign4/task3/wordstats.c
@@ -2,37 +2,37 @@
 #include <string.h>
 #define MAX_BUF 1024
 
+int main () {
 
-int main()
-{
-    /* Zero out the array */
-    int letter_frequency[26] = {0};
-    
-    int len,i,flag = 0;
-    char buf[MAX_BUF];
-    
-    do {
-        
-       
-        if (scanf("%[^\n]s", buf) == 1) len = strlen(buf); 
-        else len = 0; 
-        
-       
-        for (int j = 0; j < len; ++j){
-            
-        if (buf[j] >= 'A' && buf[j] <= 'Z') letter_frequency[buf[j] - 'A']++;
-            else if (buf[j] >= 'a' && buf[j] <= 'z') letter_frequency[buf[j] - 'a']++;
-}
-        getchar();
-        
-    } while(len > 0);
-    
-    printf("Distribution of letters in corpus:\n");
-    
-    for(i = 0; i<26; ++i){
-        // ASCII code of 'A' is 65.
-        printf("%c : %d\n", (65 + i), letter_frequency[i]);
+  /* Zero out the array */
+  int letter_frequency[26] = {0};
+  int len, i;
+  char buf[MAX_BUF];
+  char temp[MAX_BUF];
+  do{
+    fgets(temp, MAX_BUF, stdin);
+    strcat(buf, temp);
+  }while(strlen(temp)!=1);
+
+  len = strlen(buf);
+  i = 0;
+  do {
+    if(buf[i]>= 'A' && buf[i] <= 'Z'){
+      int n = buf[i]-65;
+      letter_frequency[n]++;
     }
-    
-    return 0;
+    else if (buf[i]>= 'a' && buf[i] <= 'z'){
+      int n = buf[i]-97;
+      letter_frequency[n]++;
+    }
+    len--;
+    i++;
+  } while (len > wo1);
+
+  printf("Distribution of letters in corpus:\n");
+  for (i = 0; i < 26; i++) {
+    // 'A' is ASCII code 65
+    printf("%c: %d\n", 65+i, letter_frequency[i]);
+  }
+  return 0;
 }
--- assign4/task4/bubble.c
+++ assign4/task4/bubble.c
@@ -2,25 +2,56 @@
 
 #include <stdio.h>  /* Need for standard I/O functions */
 #include <string.h> /* Need for strlen() */
+#include <stdlib.h>
 
 #define NUM 30   /* number of strings */
 #define LEN 1200 /* max length of each string */
+int s = 0;
+int n = 0;
+
+int compare(char t[],char b[]){
+  size_t lent = strlen(t)-1,lenb=strlen(b)-1;
+  if(lent-lenb>0){
+    for(s = 0;s < lenb;s++){
+      if(t[s] < b[s]) return -1;
+      else if(t[s] > b[s])return 1;
+    }
+    return 1;
+  }
+  else {
+    for(s = 0;s < lenb;s++){
+      if(t[s] < b[s]) return -1;
+      else if(t[s] > b[s])return 1;
+    }
+    return -1;
+  }
+}
 
 int main() {
   char *Strings[NUM];
-  char tmp[NUM][LEN];
+  char temp[NUM][LEN];
   printf("Please enter %d strings, one per line:\n", NUM);
   /* Write a for loop here to read NUM strings.
   Use fgets(), with LEN as an argument to ensure that an input line that is too long does not exceed the bounds imposed by the string's length. Note that the newline and NULL characters will be included in LEN.
   */
-  int i = 0;
-  for (i = 0; i < NUM; i++) {
-    fgets(Strings[i], LEN, stdin);
-  }
+  
+  for(s = 0;s < NUM;s++){
+    Strings[s]=fgets(temp[s], LEN, stdin);
+    }
+    
   puts("\nHere are the strings in the order you entered:");
   /* Write a for loop here to print all the strings. */
-  for (i = 0; i < NUM; i++) {
-    printf("[%d] %s\n", i + 1, Strings[i]);
+  for(s = 0;s < NUM;s++){
+     printf("%s",Strings[s]);
+  }
+  for(s = 0;s < NUM;s++){
+    for(n = 0;n < NUM-1-s;n++){
+      if(compare(Strings[n],Strings[n+1])==1){
+        char *q=Strings[n];
+        Strings[n]=Strings[n+1];
+        Strings[n+1]=q;
+      }
+    }
   }
   /* Bubble sort */
   /* Write code here to bubble sort the strings in ascending alphabetical order Your code must meet the following requirements:
@@ -31,23 +62,13 @@ That is, write your own while/for loop to do this.
 That is, write your own while/for loop to do this.
 (iii) You are allowed to use strlen() to calculate string lengths.
 */
-  int j;
-char temp[LEN];
-for (i = 0; i < NUM; i++) {
-    for (j = 0; j < NUM - i - 1; j++) {
-      if (strcmp(Strings[j], Strings[j + 1]) > 0) {
-        strcpy(temp, Strings[j]);
-        strcpy(Strings[j], Strings[j + 1]);
-        strcpy(Strings[j + 1], temp);
-      }
-    }
-  }
   /* Output sorted list */
   puts("\nIn alphabetical order, the strings are:");
   /* Write a for loop here to print all the strings. Feel free to use
   puts/printf etc. for printing each string.
   */
-  for (i = 0; i < NUM; i++) {
-    printf("[%d] %s\n", i + 1, Strings[i]);
+  for (s = 0; s < NUM; s++) {
+    printf("[%d] %s\n", s + 1, Strings[s]);
   }
+  return 0;
 }
